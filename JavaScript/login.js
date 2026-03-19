31d15ea15d4f4c30d7a2ac7900ad9805b524656a
warith-5ays <ademobi41@gmail.com>
2026-03-19 15:13:36 +0100

    All files added

---
+++ JavaScript/login.js
@@ -0,0 +1,36 @@
+const passwordInput = document.getElementById("passwordInput");
+const togglePassword = document.getElementById("togglePassword");
+const form = document.getElementById("loginForm");
+
+// toggle password visibility
+togglePassword.addEventListener("click", () => {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    togglePassword.classList.remove("ph-eye-slash");
+    togglePassword.classList.add("ph-eye");
+  } else {
+    passwordInput.type = "password";
+    togglePassword.classList.remove("ph-eye");
+    togglePassword.classList.add("ph-eye-slash");
+  }
+})
+
+// Handle form submission
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // stop page refresh
+
+  const email = document.getElementById("emailInput").value;
+  const password = passwordInput.value;
+
+  //simple validation
+  if (email === "" || password === "") {
+    alert ("please fill in all fields");
+    return;
+  }
+
+  //simulate login success
+  alert("Login successful!");
+
+  //Redirect (optional)
+  window.location.href = "dashboard.html";
+});
