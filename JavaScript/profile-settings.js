601bb53588fc8d6eb36387f26fad03d3a246abec
Adefaj <adefajo2000@gmail.com>
2026-03-26 02:42:51 +0100

    Fixed Responisiveness, Fixed Upload image, user can now upload image

--- JavaScript/profile-settings.js
+++ JavaScript/profile-settings.js
@@ -1,1 +1,25 @@
 //Profile Settings
+
+const uploadBtn = document.getElementById("upload");
+const fileInput = document.getElementById("fileInput");
+const preview = document.getElementById("profilePreview");
+
+// Open file picker when button is clicked
+uploadBtn.addEventListener("click", () => {
+  fileInput.click();
+});
+
+// Show preview when image is selected
+fileInput.addEventListener("change", () => {
+  const file = fileInput.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      preview.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  }
+});
